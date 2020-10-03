52cbb23541937f889b1dcd2d431277e96a81811e
Margaret Seiche <margaretseiche@hotmail.com>
2020-10-03 14:53:04 -0400

    Updates

---
+++ precache-manifest.8632b6da207cc5abd4053956fc6a1f3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c541a04b99d44973743e1e4fd745e9",
+    "url": "/2/index.html"
+  },
+  {
+    "revision": "d138d5e05013f90277c7",
+    "url": "/2/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7b8e9c744bf55d28c6fb",
+    "url": "/2/static/css/main.c57292c0.chunk.css"
+  },
+  {
+    "revision": "d138d5e05013f90277c7",
+    "url": "/2/static/js/2.6d17e210.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2/static/js/2.6d17e210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8e9c744bf55d28c6fb",
+    "url": "/2/static/js/main.a4e29b10.chunk.js"
+  },
+  {
+    "revision": "22d90fea7d80d8aff9cb",
+    "url": "/2/static/js/runtime-main.1bdfd1e0.js"
+  },
+  {
+    "revision": "35dc7a6cc7e0fe004942ec88d8df98e7",
+    "url": "/2/static/media/office_cropped.35dc7a6c.JPG"
+  }
+]);
